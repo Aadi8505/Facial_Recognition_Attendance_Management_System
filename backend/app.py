91b7f549132db435cf038d754a2bd2cbc0025d72
Aaditya Kumar <aaditya22475@gmail.com>
2025-11-14 15:38:53 +0530

    Made the facial recognition model working, which marks attendance of students in attendance table, frontend not displaying attendance marked message

--- backend/app.py
+++ backend/app.py
@@ -370,7 +370,7 @@ if __name__ == "__main__":
         db.create_all()
     app.run(debug=True)
 
-git add .
-git commit -m "Made the facial recognition model working, which marks attendance of students in attendance table, frontend not displaying attendance marked message"
-git push
+# git add .
+# git commit -m "Made the facial recognition model working, which marks attendance of students in attendance table, frontend not displaying attendance marked message"
+# git push
 
